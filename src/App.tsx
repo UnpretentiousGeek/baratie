f8f79e5f9a3ea29e6de9157201d069f89d168e65
Actionary5 <tomar.sanchit15@gmail.com>
2025-11-12 14:49:26 -0800

    Updated Logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
-import { RecipeProvider } from './context/RecipeContext';
+import { RecipeProvider, useRecipe } from './context/RecipeContext';
 import BackgroundBlobs from './components/BackgroundBlobs';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import './App.css';
 
-const App: React.FC = () => {
+const AppContent: React.FC = () => {
+  const { currentStage } = useRecipe();
+  const isCookingMode = currentStage === 'cooking';
+  
   return (
-    <RecipeProvider>
-      <BackgroundBlobs />
+    <>
+      {!isCookingMode && <BackgroundBlobs />}
       <div className="app-container">
         <Header />
         <Hero />
       </div>
+    </>
+  );
+};
+
+const App: React.FC = () => {
+  return (
+    <RecipeProvider>
+      <AppContent />
     </RecipeProvider>
   );
 };
