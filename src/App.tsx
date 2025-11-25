8bdf1a929935536e4b5c523bbe1a8ff96b7e7a6a
Actionary5 <tomar.sanchit15@gmail.com>
2025-11-24 20:17:38 -0800

    feat: add main application component, setting up routing and recipe context.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { RecipeProvider, useRecipe } from './context/RecipeContext';
 import BackgroundBlobs from './components/BackgroundBlobs';
 import Header from './components/Header';
 import Hero from './components/Hero';
-import './App.css';
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './components/About';
+import './App.css';
 
 const AppContent: React.FC = () => {
   const { currentStage } = useRecipe();
+  const location = useLocation();
   const isCookingMode = currentStage === 'cooking';
+  const isAboutPage = location.pathname === '/about';
 
   return (
-    <Router>
-      {!isCookingMode && <BackgroundBlobs />}
+    <>
+      {(!isCookingMode || isAboutPage) && <BackgroundBlobs />}
       <div className="app-container">
         <Header />
         <Routes>
@@ -22,17 +23,18 @@ const AppContent: React.FC = () => {
           <Route path="/about" element={<About />} />
         </Routes>
       </div>
-    </Router>
+    </>
   );
 };
 
 const App: React.FC = () => {
   return (
     <RecipeProvider>
-      <AppContent />
+      <Router>
+        <AppContent />
+      </Router>
     </RecipeProvider>
   );
 };
 
 export default App;
-
