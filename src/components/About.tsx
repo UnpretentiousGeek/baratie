885e2f5a6a40a36e73ad5c24aa6f9177f7d3cdee
Actionary5 <tomar.sanchit15@gmail.com>
2025-11-24 20:09:38 -0800

    feat: Add About page component with content and styling.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { ArrowLeft } from 'lucide-react';
 import './About.css';
 
 const About: React.FC = () => {
     return (
         <div className="about-container">
             <div className="about-content">
+                <Link to="/" className="back-button">
+                    <ArrowLeft size={24} />
+                </Link>
                 <div className="about-text">
                     <h1 className="about-title">About Baratie</h1>
                     <div className="about-description">
-                        <p>Baratie is an open source recipe manager built with the help of Cursor and Claude Code.</p>
+                        <p>Hey,</p>
+                        <p className="spacer">&nbsp;</p>
+                        <p>Thank you for checking out Baratie. I built this project as a way to gain some experience building Products and also to add it to my portfolio. I want to get into Product Design but have no experience in it. It was a great experience designing it and developing it. I mostly used Figma for design and then vibe coded this. For vibe coding I used Cursor and Claude Code.</p>
                         <p className="spacer">&nbsp;</p>
                         <p>Thanks</p>
                     </div>
